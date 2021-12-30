3ebc2303b1143d7eaf22b7138f4b9c08c2c3df04
manas140 <64472354+Manas140@users.noreply.github.com>
2021-12-30 13:43:48 +0000

    font_path fix

--- install.sh
+++ install.sh
@@ -3,11 +3,6 @@
 cr="\033[1;31m"
 cg="\033[1;32m"
 cb="\033[1;34m"
-case $(uname -s) in 
-  Linux*) fontdir=$HOME/.fonts/.;;
-  Darwin*) fontdir=$HOME/Library/Fonts/.;;
-  *) printf "${cr}Error: Os Not Supported";;
-esac
 printf "${cr}"
 case $1 in 
   *u*) sudo rm -r /usr/local/bin/fp && printf "${cg}[*] FP Uninstalled\n${cr}";;
